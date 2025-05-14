78e995c51507f497e1064f1bd43d38d92c61eafe
Xaxxoo <bnahmad83@gmail.com>
2025-05-14 17:15:51 +0100

    Some Files Deleted

--- hello_world.sol
+++ hello_world.sol
@@ -3,11 +3,11 @@
 pragma solidity >= 0.7.0 < 0.9.0;
 
 contract ThisIsAContract{
-    constructor() public {}
+    // constructor() public {}
 
     function getResult() public pure returns(uint){
-        uint a = 1;
-         uint b = 2;
+        uint a = 10;
+         uint b = 5;
           uint result = a+b;
           return result;
     }
---
+++ variables.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity >= 0.7.0 < 0.9.0;
+
+contract LearnVariables {
+
+
+    //Intergers: Numbers (Signed or Unsigned)
+
+        uint chocolateBar = 10;
+        uint storeOwner = 300;
+
+    //Booleans: Checks if something is true or false
+
+    bool lieDetector = true;
+
+
+    //Strings: Are data values that are made of ordered sequence of characters (Expensive)
+        string errorMessage = "Error! There has been a mistake!";
+    
+}
